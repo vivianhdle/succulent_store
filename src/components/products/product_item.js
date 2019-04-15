4aebe2f3711c95802bea3133854dc6a1d1b219e7
Vivian Le <vivianhdle@gmail.com>
2019-04-14 21:22:26 -0700

    added more css to products page, added helper functions

--- src/components/products/product_item.js
+++ src/components/products/product_item.js
@@ -1,25 +1,29 @@
 import React, {Component} from 'react';
+import {formatMoney} from '../../helpers';
 
 class ProductItem extends Component{
+    componentDidMount(){
+        M.Materialbox.init(this.zoom)
+    }
     render(){
         console.log(this.props);
         const {id,images,name,price}=this.props.info
         const url = `/dist/${images}`
-        return(          
-                <div className="col s12 m4">
-                    <div className="card">
-                        <div className="card-image">
-                            <img src={url} alt=""/>
-                            <span className="card-title">{name}</span>
-                        </div>
-                        <div className="card-content">
-                            <p>{price}</p>
-                        </div>
-                        <div className="card-action">
-                            <a href="#">More Info</a>
-                        </div>
+        return(        
+            <div className="col s12 m4 card-item">
+                <div className="card col s12 m10 offset-m1">
+                    <div className="card-image">
+                        <img ref={(element)=>this.zoom=element} className="materialboxed" src={url} alt=""/>
+                        <span className="card-title">{name}</span>
+                    </div>
+                    <div className="card-content">
+                        <p>{formatMoney(price)}</p>
+                    </div>
+                    <div className="card-action">
+                        <a href="#">More Info</a>
                     </div>
                 </div>
+            </div>
         )
     }
 }

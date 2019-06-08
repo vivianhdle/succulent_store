cb9ee70d35893ac8766debaccffabb723560d786
Vivian Le <vivianhdle@gmail.com>
2019-06-08 14:09:49 -0700

    added soil care guide for buyers

--- src/components/product_care/index.js
+++ src/components/product_care/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import WaterCare from './water_care';
 import TableOfContents from './table_of_contents';
 import './product_care.scss';
+import SoilCare from './soil_care';
 
 function ProductCare(){
     return(
         <div className="care-container">
             <TableOfContents/>
             <WaterCare/>
+            <SoilCare/>
         </div>
     )
 }
